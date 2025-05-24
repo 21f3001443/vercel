cc02b258b42d34779f364be93b2281159062d6a6
21f3001443 <96081427+21f3001443@users.noreply.github.com>
2025-05-25 03:05:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import json
+from typing import List
 
 app = FastAPI()
 
 @app.get("/api")
-def read_root():
+def read_root(filter_names: List[str] = Query(...)):
     data = ''
     with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
 
     output = {
-        "marks": [entry["marks"] for entry in data]
+        "marks": [entry["marks"] for entry in data if entry["name"] in filter_names]
     }
     return json.dumps(output)
