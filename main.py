6c35899dbd255bfde9f28cb3e66f0bff6c9684dc
21f3001443 <96081427+21f3001443@users.noreply.github.com>
2025-05-25 13:17:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ def read_root(name: list[str] = Query(...)):
     headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
     }
     return Response(content=out, status_code=200, headers=headers)
