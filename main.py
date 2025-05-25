2617e4532270ca1278de621eaa7b4dc205523b47
21f3001443 <96081427+21f3001443@users.noreply.github.com>
2025-05-25 12:37:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import json
 
 app = FastAPI()
 
 @app.get("/")
-def read_root(name: Query(...)):
+def read_root(name: ['tpQ', '2UOWSLOtV1']):
     with open('q-vercel-python.json', 'r') as f:
         data = json.load(f)
 
